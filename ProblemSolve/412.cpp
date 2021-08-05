7ba6f9ddccc93cdc554787656272eebba8de4b08
soonmin-kwon <tnsals0709@naver.com>
2021-08-05 17:59:55 +0900

    Easy, Simulation, Naive

---
+++ ProblemSolve/412.cpp
@@ -0,0 +1,29 @@
+/*************
+ * Fizz Buzz *
+ *************/
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        ans.push_back("1");
+        
+        //처음부터 끝까지 값 입력
+        for(int i=1; i<n; i++){
+            if((i + 1) % 3 == 0 && (i + 1) % 5 == 0){
+                ans.push_back("FizzBuzz");
+            }
+            else if((i + 1) % 3 == 0){
+                ans.push_back("Fizz");
+            }
+            else if((i + 1) % 5 == 0){
+                ans.push_back("Buzz");
+            }
+            else{
+                ans.push_back(to_string(i + 1));
+            }
+        }
+        
+        return ans;
+    }
+};
