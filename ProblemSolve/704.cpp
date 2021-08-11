786436a28e12d3cde580b056f3854bd83d6a15b5
soonmin-kwon <tnsals0709@naver.com>
2021-08-11 23:04:43 +0900

    Easy, Binary Search

---
+++ ProblemSolve/704.cpp
@@ -0,0 +1,29 @@
+/*****************
+ * Binary Search *
+ *****************/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        int mid;
+        
+        //binary search
+        while(left <= right){
+            int mid = (left + right)/2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                right = mid -1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        
+        return -1;
+    }
+};
