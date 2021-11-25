46b00a61b95d3a3257d70530c68e0176c0aee705
soonmin-kwon <tnsals0709@naver.com>
2021-11-25 22:37:35 +0900

    잘못된 스크립트 수정

--- ProblemSolve/53.cpp
+++ ProblemSolve/53.cpp
@@ -2,25 +2,19 @@
  * Maximum Subarray *
  ********************/
 
-// Kadane 알고리즘
-// O(n)의 속도 만족
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int len = nums.size();
-        if(len == 1){
-            return nums[0];
+        int ans = -10001;
+        int tempSubarray = 0;
+        
+        //Kadane Algorithm
+        for(int i=0; i<nums.size(); ++i){
+            tempSubarray += nums[i];
+            ans = max(ans, tempSubarray);
+            tempSubarray = max(tempSubarray, 0);
         }
         
-        int sum = 0;
-        int ans = -100001;
-        
-        for(int i=0; i<len; i++){
-            ans = max(ans, sum);
-            sum = max(sum,0);
-        }
-        
-        return sum;
+        return ans;
     }
 };
