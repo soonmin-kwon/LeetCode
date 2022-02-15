67b77173d3590f3b8c937bf1f1897d8487d9010e
soonmin-kwon <65493195+soonmin-kwon@users.noreply.github.com>
2022-02-15 21:27:52 +0900

    Easy,

---
+++ ProblemSolve/136.cs
@@ -0,0 +1,14 @@
+/*****************
+ * Single Number *
+ *****************/
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int ans = 0;
+        foreach(int num in nums){
+            ans ^= num;
+        }
+        
+        return ans;
+    }
+}
