97e61978b6506bf7f1743b23ffebc450469db9d7
soonmin-kwon <65493195+soonmin-kwon@users.noreply.github.com>
2022-03-28 21:51:11 +0900

    Medium, Binary Search

---
+++ ProblemSolve/81.cpp
@@ -0,0 +1,44 @@
+/*************************************
+ * Search in Rotated Sorted Array II *
+ *************************************/
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        
+        while(left <= right){
+            while(left < right && nums[left] == nums[left + 1]){
+                left++;
+            }
+            
+            while(left < right && nums[right] == nums[right - 1]){
+                right--;
+            }
+            
+            int mid = (right + left) / 2;
+            
+            if(nums[mid] == target){
+                return true;
+            }
+            else if(nums[mid] >= nums[left]){
+                if(nums[left] <= target && nums[mid] >= target){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && nums[right] >= target){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
